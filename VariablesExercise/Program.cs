af40b73703b55bef7fce4af377b3f4fba45c9e8b
Adam Jankowski <ghost17814@gmail.com>
2023-04-07 14:22:32 -0400

    The Zach saga continues

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string slothName = "Zach";
+            int howManyPeopleAreFasterThanZach = 0;
+            char yippy = '!';
+            bool isTheFastest = true;
+            double mph = 120.23;
+            decimal kph = 193.491429m;
+            Console.WriteLine($"I know of a sloth named {slothName} and it is {isTheFastest} that he the fastest being \n" +
+                $"on the planet, there are exactly {howManyPeopleAreFasterThanZach} people faster than him. He was once clocked \n" +
+                $"going {mph} mph or {kph} kph{yippy}");
         }
     }
 }
